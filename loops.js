a77b0f59d9d06341345fcd021cdbc7583fb15919
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-09-26 22:51:40 +0530

    Javascript

---
+++ loops.js
@@ -0,0 +1,33 @@
+// let a=20;
+// let b=50;
+//     if (a>b){
+//         console.log("a is smaller ");
+//     }
+// if (a<b){
+//     console.log("b is bigger  ");
+// }
+
+// const num=99;
+// if (num %2 ===0){
+//     console.log("EVen");
+// }else{
+//     console.log("ODD")
+// }
+
+// let age =prompt ("Enter the age "); age =number (age );
+// if (age<18){
+//     console.log("You cannot enter!!");
+// }
+// else if (age >=18 && age <25){
+// console.log("You can enter but cannot drink ");
+// }
+// else{
+//     console.log("You can enter and drink ");
+// }
+
+for (let num =1; num<=10;num ++){
+    if (num===5){
+        break;
+    }
+    console.log(num);
+}
