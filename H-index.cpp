84638f178eeb60c0342d001b5dcce045042af6d1
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-09-29 21:34:23 +0530

    mon

---
+++ H-index.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        int h = 0;
+        for (int i = 0; i < citations.size(); i++) {
+            if (citations[i] >= i + 1) {
+                h++;
+            } else {
+                break;
+            }
+        }
+        return h;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    // Example input
+    vector<int> citations = {3, 0, 6, 1, 5};
+    
+    // Call function
+    int result = sol.hIndex(citations);
+    
+    cout << "H-Index: " << result << endl;
+    return 0;
+}
