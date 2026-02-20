bf6ce6bf3789c6bcc1ba45012b4942b6155ae1c1
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-02-20 10:51:56 +0530

    update

--- recursion/function.cpp
+++ recursion/function.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    int add(int a, int b){
+    int add(int a, int b){ //function declaration
     return a+b;
 }
 
 int main(){
-cout<<add(3,4);
+cout<<add(3,4);  //function call
 return 0;
 }
